2411369c9bb7e1d96ad1f0a87b40244ae8c5d99b
hodakhanoom <hoda.khanoom.rad@gmail.com>
2024-01-19 13:15:08 +0330

    debugging

--- helper.py
+++ helper.py
@@ -3,13 +3,13 @@ import tweepy
 import re
 from wordcloud import WordCloud
 
-def authenticate(consumer_key, consumer_secret,access_token_key, access_token_secret):    
+def authenticate(consumer_key, consumer_secret,access_token, access_token_secret):    
     #auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     #auth.set_access_token(access_token_key, access_token_secret)
     auth = tweepy.OAuth1UserHandler(
     consumer_key,
     consumer_secret,
-    access_token_key,
+    access_token,
     access_token_secret,
     )
     api = tweepy.API(auth)
